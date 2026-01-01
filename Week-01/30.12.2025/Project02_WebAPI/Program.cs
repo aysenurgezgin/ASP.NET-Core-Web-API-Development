05ac4de1d57f285aeeca124b0573b444b0a50dcf
aysenurgezgin <aysenurgezgin99@gmail.com>
2026-01-01 21:36:59 +0300

    Update

--- Week-01/30.12.2025/Project02_WebAPI/Program.cs
+++ Week-01/30.12.2025/Project02_WebAPI/Program.cs
@@ -1,21 +1,22 @@
-var builder = WebApplication.CreateBuilder(args);
+var builder = WebApplication.CreateBuilder(args); //1.Aşama
 
 builder.Services.AddControllers();//Controllerlerı kattıık ama gidip kendimiz de controller oluşturduk.
 builder.Services.AddEndpointsApiExplorer(); //uyg.tüm controlerları bizim için gezecek keşif için(methot tiplerine bakacak,hangi urlden istek yapılması halinde çalışacak tespit ve sonrasında bilgileri kayıtlı tutar ve sonra )
 builder.Services.AddSwaggerGen(); //Diyerekte o topladığı bilgilerle endpointlerle bu uygulamayı test edebileceğim dökümente edebileceğim bir web sayfası üret,uygulama çalışınca ben o sayfayı görebileyim.
 
-var app = builder.Build();
+var app = builder.Build(); //2.Aşama
 
+app.UseSwagger(); //endpoint vs tespit işlemeleri
 
-if (app.Environment.IsDevelopment()) //Eğer geliştirme ortamında çalışıyorsan  uygulama olarak bunu kullan demek.Production yaptığında(yayım)swagerla ilgili kısmı istemiyeceğim çünkü
-{
-    app.UseSwagger(); //endpoint vs tespit işlemeleri
-    app.UseSwaggerUI();//o web sayfasını üret
-}
+app.UseSwaggerUI();//o web sayfasını üret
 
 app.UseHttpsRedirection();
 
+app.UseAuthentication();
+
+app.UseAuthorization();
+
 app.MapControllers(); 
 
-app.Run();
+app.Run(); //3.Aşama
 
