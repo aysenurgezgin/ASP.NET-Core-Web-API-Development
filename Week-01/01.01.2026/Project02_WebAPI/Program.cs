3bb687fe2caa34fcf3efd136d09bc8e31f67322c
aysenurgezgin <aysenurgezgin99@gmail.com>
2026-01-01 23:42:25 +0300

    newProject-EShop

--- Week-01/01.01.2026/Project02_WebAPI/Program.cs
+++ Week-01/01.01.2026/Project02_WebAPI/Program.cs
@@ -1,8 +1,26 @@
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.OpenApi;
+
 var builder = WebApplication.CreateBuilder(args); //1.Aşama
 
 builder.Services.AddControllers();//Controllerlerı kattıık ama gidip kendimiz de controller oluşturduk.
 builder.Services.AddEndpointsApiExplorer(); //uyg.tüm controlerları bizim için gezecek keşif için(methot tiplerine bakacak,hangi urlden istek yapılması halinde çalışacak tespit ve sonrasında bilgileri kayıtlı tutar ve sonra )
-builder.Services.AddSwaggerGen(); //Diyerekte o topladığı bilgilerle endpointlerle bu uygulamayı test edebileceğim dökümente edebileceğim bir web sayfası üret,uygulama çalışınca ben o sayfayı görebileyim.
+builder.Services.AddSwaggerGen(opt =>
+{
+    opt.SwaggerDoc("v1",new OpenApiInfo
+    {
+        Title="Sample WebAPI Project",
+        Version="v1",
+        Description="WebAPI yapısı için oluşturuldu.",
+        Contact=new OpenApiContact
+        {
+            Name="Aylin Sezer",
+            Email="info@aylinszr.com"
+        }
+    });
+}
+
+);
 
 var app = builder.Build(); //2.Aşama
 
@@ -20,3 +38,4 @@ app.MapControllers();
 
 app.Run(); //3.Aşama
 
+//AddSwaggerGen=Diyerekte o topladığı bilgilerle endpointlerle bu uygulamayı test edebileceğim dökümente edebileceğim bir web sayfası üret,uygulama çalışınca ben o sayfayı görebileyim.
